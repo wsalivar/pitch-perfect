ed0e5a3e4d33e83b098b157a1672e42bf04cc144
wsalivar <wsalivar@users.noreply.github.com>
2015-03-10 17:30:24 -0700

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by William Salivar on 3/5/15.
+//  Copyright (c) 2015 William Salivar. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject
+{
+    var filePathUrl: NSURL!
+    var title: String!
+}
